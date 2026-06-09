53209418300b6fdddc378a06c2e2ac67346dce3c
AKapin5 <akapin@architeq.ch>
2026-06-09 17:52:12 +0300

    rewrite step 5

--- components/MortageForm/Steps/FifthStep.tsx
+++ components/MortageForm/Steps/FifthStep.tsx
@@ -1,54 +1,48 @@
 import React from "react";
 import DatePicker from "react-date-picker";
-import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 
-import { IpersonInfo } from "../../../interfaces";
-import { setPersonData } from "../../../redux/actions/setPersonData";
-import { setFormStep } from "redux/actions/setFormStage";
-import { setError } from "..";
-
-import { Stepper } from "../Stepper";
-
 import userIcon from "../../../assets/images/dist/rechner/user-icon.png";
-import checkIcon from "../../../assets/images/dist/rechner/checkbox-check-icon.png";
 
-import { sendMetrik } from "../../utils/GoogleAnalytics";
+import { IpersonInfo } from "../../../interfaces";
 
-interface Iprops {
+interface Props {
   hidden: boolean;
-}
 
-interface IjsonBody {
-  anrede: string;
-  name: string;
-  lastname: string;
-  birthday: string;
-  email: string;
-  phone: string;
-  streetAndHouseNumber: string;
-  plz: number;
-  city: string;
-  work: string;
-  message: string;
-  married: boolean;
-  children: boolean;
-  childrenCount: number;
-  foundUs: string;
-  income: number;
-  calculated: number;
-  tableData: {
-    Jahren: number;
-    Zinsen: number;
-    Tilgung: number;
-    Annuitaet: number;
-    Darlehensrest: number;
+  personFamilyInfo: {
+    marriaged: boolean;
+    kids: number;
   };
-  immoCost: number;
+
+  personSalary: number;
+  personSource: string;
+
+  finalMortage: number;
+  tableData: any;
+  mortageDuration: number;
+  propertyValue: number;
+
+  onNextStep: () => void;
+  onPrevStep: () => void;
+  setError: (msg: string) => void;
+  setPersonData: (data: IpersonInfo) => void;
 }
 
-export const FifthStep = React.memo(({ hidden }: Iprops) => {
-  const dispatch = useDispatch();
+export const FifthStep = React.memo((props: Props) => {
+  const {
+    hidden,
+    personFamilyInfo,
+    personSalary,
+    personSource,
+    finalMortage,
+    tableData,
+    mortageDuration,
+    propertyValue,
+    onNextStep,
+    onPrevStep,
+    setError,
+    setPersonData,
+  } = props;
 
   const initialDate = new Date().setFullYear(
     new Date().getFullYear() - 18
@@ -69,19 +63,6 @@ export const FifthStep = React.memo(({ hidden }: Iprops) => {
     ort: "",
     additionalInfo: "",
   });
-
-  const {
-    personFamilyInfo,
-    personSalary,
-    personSource,
-  } = useSelector((state: any) => state.personDateReducer);
-
-  const {
-    finalMortage,
-    tableData,
-    mortageDuration,
-    propertyValue,
-  } = useSelector((state: any) => state.mortageInfoReducer);
 
   const handleInput = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -118,13 +99,12 @@ export const FifthStep = React.memo(({ hidden }: Iprops) => {
     if (el) window.scrollTo(0, el.offsetTop);
   };
 
-  const handleBackBtn = () => {
-    sendMetrik("BackToThirdStep", "MortageForm", "BackButtonPressed", "");
-    dispatch(setFormStep(4));
+  const handleBack = () => {
+    onPrevStep();
     scrollToForm();
   };
 
-  const handleContinueBtn = () => {
+  const handleContinue = () => {
     const validateEmail = (email: string) =>
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@/.test(email);
 
@@ -132,28 +112,26 @@ export const FifthStep = React.memo(({ hidden }: Iprops) => {
       /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s./0-9]*$/g.test(phone);
 
     if (mortageData.name.length < 2)
-      return setError(dispatch, "„Vorname“ ausfüllen");
+      return setError("„Vorname“ ausfüllen");
 
     if (mortageData.surname.length < 2)
-      return setError(dispatch, "„Nachname“ ausfüllen");
+      return setError("„Nachname“ ausfüllen");
 
     if (!validatePhone(mortageData.phone) || mortageData.phone.length < 7)
-      return setError(dispatch, "„Telefon“ ausfüllen");
+      return setError("„Telefon“ ausfüllen");
 
     if (!mortageData.birthday)
-      return setError(dispatch, "„Geburtsdatum“ ausfüllen");
+      return setError("„Geburtsdatum“ ausfüllen");
 
     if (!validateEmail(mortageData.email))
-      return setError(dispatch, "„E-Mail“ ausfüllen");
+      return setError("„E-Mail“ ausfüllen");
 
     if (!isConf)
-      return setError(dispatch, "Alle Felder sind erforderlich");
-
-    sendMetrik("LastStepPassed", "MortageForm", "SendButtonPressed", "");
+      return setError("Alle Felder sind erforderlich");
 
     const kids = personFamilyInfo?.kids || 0;
 
-    const payload: IjsonBody = {
+    const payload = {
       anrede: mortageData.gender,
       name: mortageData.name,
       lastname: mortageData.surname,
@@ -189,128 +167,118 @@ export const FifthStep = React.memo(({ hidden }: Iprops) => {
           headers: {
             "Content-Type": "application/json",
             Authorization:
-              "Bearer e8956e92833ec3a9a9a8283797ed3390c37971f809b914ca01beeca682f275edb262a1baab7ad77fb332a81a4b2efcfd62016d65ef66da06642cb71775562594",
+              "Bearer YOUR_TOKEN_HERE",
           },
         }
       )
       .then(() => {
-        dispatch(setFormStep(6));
+        onNextStep();
         scrollToForm();
       })
       .catch((err) => {
         console.error(err);
-        setError(dispatch, "Auf der Serverseite ist ein Fehler aufgetreten");
+        setError("Serverfehler beim Senden");
       });
   };
 
   if (hidden) return null;
 
   return (
-    <form className="mortgage-form" id="formHeading">
+    <form className="mortgage-form">
       <div className="container">
-        <Stepper />
+        <h2 className="h2">Persönliche Angaben</h2>
 
-        <div className="form-wrapper ml-9">
-          <h2 className="h2">Persönliche Angaben</h2>
+        <div className="body-content">
+          <img src={userIcon} alt="User icon" />
 
-          <div className="body-content">
-            <img src={userIcon} alt="User icon" />
-
-            <div className="form-info-container">
-              {/* Gender */}
-              <div className="radio-block">
-                <span>Anrede</span>
-
-                <label>
-                  <input
-                    type="radio"
-                    value="Herr"
-                    checked={mortageData.gender === "Herr"}
-                    onChange={handleGender}
-                  />
-                  Herr
-                </label>
-
-                <label>
-                  <input
-                    type="radio"
-                    value="Frau"
-                    checked={mortageData.gender === "Frau"}
-                    onChange={handleGender}
-                  />
-                  Frau
-                </label>
-              </div>
-
-              {/* Inputs */}
-              <div className="inputs">
-                <input name="name" placeholder="Vorname" onChange={handleInput} />
-                <input name="surname" placeholder="Nachname" onChange={handleInput} />
-                <input name="phone" placeholder="Telefonnummer" onChange={handleInput} />
-                <input name="email" placeholder="E-Mail" onChange={handleInput} />
-
-                <input name="strasse" placeholder="Straße" onChange={handleInput} />
-                <input name="postalCode" placeholder="PLZ" onChange={handleInput} />
-                <input name="ort" placeholder="Ort" onChange={handleInput} />
-
-                <DatePicker
-                  value={mortageData.birthday}
-                  maxDate={new Date(initialDate)}
-                  onChange={handleDateInput}
-                />
-              </div>
-
-              {/* Employment */}
-              <div className="radio-block">
-                <label>
-                  <input
-                    type="radio"
-                    value="Angestellter"
-                    checked={mortageData.employment === "Angestellter"}
-                    onChange={handleEmployment}
-                  />
-                  Angestellter
-                </label>
-
-                <label>
-                  <input
-                    type="radio"
-                    value="Selbständiger"
-                    checked={mortageData.employment === "Selbständiger"}
-                    onChange={handleEmployment}
-                  />
-                  Selbständiger
-                </label>
-              </div>
-
-              {/* Message */}
-              <textarea
-                name="additionalInfo"
-                placeholder="Nachricht"
-                onChange={handleInput}
+          {/* Gender */}
+          <div className="radio-block">
+            <label>
+              <input
+                type="radio"
+                value="Herr"
+                checked={mortageData.gender === "Herr"}
+                onChange={handleGender}
               />
+              Herr
+            </label>
 
-              {/* Consent */}
-              <label className="consent">
-                <input
-                  type="checkbox"
-                  checked={isConf}
-                  onChange={() => setIsConf(!isConf)}
-                />
-                Ich stimme der Verarbeitung zu
-              </label>
-            </div>
+            <label>
+              <input
+                type="radio"
+                value="Frau"
+                checked={mortageData.gender === "Frau"}
+                onChange={handleGender}
+              />
+              Frau
+            </label>
           </div>
 
-          <div className="form-buttons">
-            <button type="button" onClick={handleBackBtn}>
-              Zurück
-            </button>
+          {/* Inputs */}
+          <div className="inputs">
+            <input name="name" placeholder="Vorname" onChange={handleInput} />
+            <input name="surname" placeholder="Nachname" onChange={handleInput} />
+            <input name="phone" placeholder="Telefon" onChange={handleInput} />
+            <input name="email" placeholder="E-Mail" onChange={handleInput} />
 
-            <button type="button" onClick={handleContinueBtn}>
-              Anfrage absenden
-            </button>
+            <input name="strasse" placeholder="Straße" onChange={handleInput} />
+            <input name="postalCode" placeholder="PLZ" onChange={handleInput} />
+            <input name="ort" placeholder="Ort" onChange={handleInput} />
+
+            <DatePicker
+              value={mortageData.birthday}
+              maxDate={new Date(initialDate)}
+              onChange={handleDateInput}
+            />
           </div>
+
+          {/* Employment */}
+          <div className="radio-block">
+            <label>
+              <input
+                type="radio"
+                value="Angestellter"
+                checked={mortageData.employment === "Angestellter"}
+                onChange={handleEmployment}
+              />
+              Angestellter
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Selbständiger"
+                checked={mortageData.employment === "Selbständiger"}
+                onChange={handleEmployment}
+              />
+              Selbständiger
+            </label>
+          </div>
+
+          <textarea
+            name="additionalInfo"
+            placeholder="Nachricht"
+            onChange={handleInput}
+          />
+
+          <label className="consent">
+            <input
+              type="checkbox"
+              checked={isConf}
+              onChange={() => setIsConf(!isConf)}
+            />
+            Ich stimme der Verarbeitung meiner Daten zu
+          </label>
+        </div>
+
+        <div className="buttons">
+          <button type="button" onClick={handleBack}>
+            Zurück
+          </button>
+
+          <button type="button" onClick={handleContinue}>
+            Anfrage absenden
+          </button>
         </div>
       </div>
     </form>
